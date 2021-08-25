1664d17c9e349e86ba81bcc313365fc057daff3f
Augusto Caceres Suarez <augustocsuarez1985@gmail.com>
2021-08-25 14:40:27 -0500

    Agregado de cors para eivtar block

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const personarutas = require('./src/routes/personas.routes')
-
+const cors = require('cors');
 const app = express()
 app.set('port',process.env.PORT || 3000)
 
@@ -9,5 +9,7 @@ app.set('port',process.env.PORT || 3000)
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(personarutas)
+app.use(cors());
+
 
 module.exports = app
